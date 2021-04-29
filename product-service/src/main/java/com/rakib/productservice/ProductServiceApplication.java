5775848f789019b8815b43840394ed7153e19ef3
Jahadul Rakib <rakibdiu2015@gmail.com>
2021-04-29 23:26:37 +0600

    add

--- product-service/src/main/java/com/rakib/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/rakib/productservice/ProductServiceApplication.java
@@ -2,14 +2,11 @@ package com.rakib.productservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
-import org.springframework.web.reactive.config.EnableWebFlux;
 
 import java.net.URI;
 import java.net.URISyntaxException;
 
-@EnableWebFlux
-@EnableReactiveMongoRepositories
+
 @SpringBootApplication
 public class ProductServiceApplication {
 
